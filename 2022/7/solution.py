8932c3d6c65527bdf10e792379445ed4ac07e88f
Delta-de-Dirac <rconceicao.lucas@gmail.com>
2023-07-28 18:47:15 -0300

    complete solution

--- 2022/7/solution.py
+++ 2022/7/solution.py
@@ -1,9 +1,15 @@
+from functools import lru_cache 
 class Directory:
-    def __init__(self, name = "", files = [], directories = [], parentDirectory = None):
+    def __init__(self, name = "", files = None, directories = None, parentDirectory = None):
+        if files == None:
+            files = list()
+        if directories == None:
+            directories = list()
         self.name = name
         self.files = files
         self.directories = directories
         self.parentDirectory = parentDirectory
+    @lru_cache(maxsize=None)
     def getSize(self):
         sizeOfFileInDiretory = 0
         sizeOfdirectories = 0
@@ -12,8 +18,20 @@ class Directory:
         for directory in self.directories:
             sizeOfdirectories += directory.getSize()
         return sizeOfdirectories + sizeOfFileInDiretory
-
-
+    def getAllDirectoriesSizeAtMost(self, n):
+        result = []
+        for directory in self.directories:
+            if directory.getSize() <= n:
+                result.append(directory)
+            result += directory.getAllDirectoriesSizeAtMost(n)
+        return result
+    def getAllDirectoriesSizeAtLeast(self, n):
+        result = []
+        for directory in self.directories:
+            if directory.getSize() >= n:
+                result.append(directory)
+            result += directory.getAllDirectoriesSizeAtLeast(n)
+        return result
 fileName = "adventofcode.com_2022_day_7_input.txt"
 root = Directory(name="/")
 currentDirectory = None
@@ -37,10 +55,23 @@ with open(file=fileName, mode="r") as f:
                     break
             continue
         if "dir " in line:
+            if any(map(lambda x: x.name == line.split(" ")[1][:-1],currentDirectory.directories)):
+                continue
             currentDirectory.directories.append(Directory(name=line.split(" ")[1][:-1], parentDirectory=currentDirectory))
             continue
-
         newFileSize = int(line.split(" ")[0])
         newFileName = line.split(" ")[1][:-1]
         currentDirectory.files.append([newFileSize, newFileName])
-print(root.getSize())
+sumDesiredSize = 0
+for directory in root.getAllDirectoriesSizeAtMost(100000):
+    sumDesiredSize += directory.getSize()
+print(sumDesiredSize)
+spaceNeeded = 30000000
+filesystemSize = 70000000
+spaceUsed = root.getSize()
+freeSpace = filesystemSize - spaceUsed
+spaceToFree = spaceNeeded - freeSpace
+smallestFound = filesystemSize
+for directory in root.getAllDirectoriesSizeAtLeast(spaceToFree):
+    smallestFound = min(directory.getSize(),smallestFound)
+print(smallestFound)
