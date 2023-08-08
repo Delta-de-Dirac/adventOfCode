da4a0341072a66c8bae420e87648e0ebe2112ec7
Delta-de-Dirac <rconceicao.lucas@gmail.com>
2023-08-08 15:35:18 -0300

    optimizing solution

--- 2022/19/solution.py
+++ 2022/19/solution.py
@@ -17,128 +17,138 @@ def getMaxGeode(time, production, stock, blueprint):
 	maxFound = 0
 	cantBuildAnything = True
 	# build ore next 
-	timeToResource = max(math.ceil((blueprint[0][0] - stock[0])/production[0]), 0)
-	waitTime = timeToResource + 1
-	if waitTime < time:
-		cantBuildAnything = False
-		maxFound = max(
-		getMaxGeode(
-			time - waitTime,
-			(
-				production[0] + 1,
-				production[1],
-				production[2],
-				production[3]
-			),
-			(
-				stock[0] + waitTime*production[0] - blueprint[0][0],
-				stock[1] + waitTime*production[1],
-				stock[2] + waitTime*production[2],
-				stock[3] + waitTime*production[3]
-			),
-			blueprint
-		),
-		maxFound
-		)
-	# build clay next 
-	timeToResource = max(math.ceil((blueprint[1][0] - stock[0])/production[0]), 0)
-	waitTime = timeToResource + 1
-	if waitTime < time:
-		cantBuildAnything = False
-		maxFound = max(
-		getMaxGeode(
-			time - waitTime,
-			(
-				production[0],
-				production[1] + 1,
-				production[2],
-				production[3]
-			),
-			(
-				stock[0] + waitTime*production[0] - blueprint[1][0],
-				stock[1] + waitTime*production[1],
-				stock[2] + waitTime*production[2],
-				stock[3] + waitTime*production[3]
-			),
-			blueprint
-		),
-		maxFound
-		)
-	# build obsidian next
-	if production[1] > 0:
+	if production[0] < max(blueprint[0][0],blueprint[1][0],blueprint[2][0],blueprint[3][0]):
 		timeToResource = max(
-			max(math.ceil((blueprint[2][0] - stock[0])/production[0]), 0),
-			max(math.ceil((blueprint[2][1] - stock[1])/production[1]), 0)
-			)
+			math.ceil((blueprint[0][0] - stock[0])/production[0]), 
+			0
+		)
 		waitTime = timeToResource + 1
 		if waitTime < time:
 			cantBuildAnything = False
 			maxFound = max(
-			getMaxGeode(
-				time - waitTime,
-				(
-					production[0],
-					production[1],
-					production[2] + 1,
-					production[3]
+				getMaxGeode(
+					time - waitTime,
+					(
+						production[0] + 1,
+						production[1],
+						production[2],
+						production[3]
+					),
+					(
+						stock[0] + waitTime*production[0] - blueprint[0][0],
+						stock[1] + waitTime*production[1],
+						stock[2] + waitTime*production[2],
+						stock[3] + waitTime*production[3]
+					),
+					blueprint
 				),
-				(
-					stock[0] + waitTime*production[0] - blueprint[2][0],
-					stock[1] + waitTime*production[1] - blueprint[2][1],
-					stock[2] + waitTime*production[2],
-					stock[3] + waitTime*production[3]
+				maxFound
+			)
+	# build clay next 
+	if production[1] < blueprint[2][1]:
+		timeToResource = max(
+			math.ceil((blueprint[1][0] - stock[0])/production[0]), 
+			0
+		)
+		waitTime = timeToResource + 1
+		if waitTime < time:
+			cantBuildAnything = False
+			maxFound = max(
+				getMaxGeode(
+					time - waitTime,
+					(
+						production[0],
+						production[1] + 1,
+						production[2],
+						production[3]
+					),
+					(
+						stock[0] + waitTime*production[0] - blueprint[1][0],
+						stock[1] + waitTime*production[1],
+						stock[2] + waitTime*production[2],
+						stock[3] + waitTime*production[3]
+					),
+					blueprint
 				),
-				blueprint
-			),
-			maxFound
+				maxFound
+			)
+	# build obsidian next
+	if production[1] > 0 and production[2] < blueprint[3][1]:
+		timeToResource = max(
+			math.ceil((blueprint[2][0] - stock[0])/production[0]),
+			math.ceil((blueprint[2][1] - stock[1])/production[1]), 
+			0
+		)
+		waitTime = timeToResource + 1
+		if waitTime < time:
+			cantBuildAnything = False
+			maxFound = max(
+				getMaxGeode(
+					time - waitTime,
+					(
+						production[0],
+						production[1],
+						production[2] + 1,
+						production[3]
+					),
+					(
+						stock[0] + waitTime*production[0] - blueprint[2][0],
+						stock[1] + waitTime*production[1] - blueprint[2][1],
+						stock[2] + waitTime*production[2],
+						stock[3] + waitTime*production[3]
+					),
+					blueprint
+				),
+				maxFound
 			)
 	# build geode next
 	if production[2] > 0:
 		timeToResource = max(
-			max(math.ceil((blueprint[3][0] - stock[0])/production[0]), 0),
-			max(math.ceil((blueprint[3][1] - stock[2])/production[2]), 0)
-			)
+			math.ceil((blueprint[3][0] - stock[0])/production[0]),
+			math.ceil((blueprint[3][1] - stock[2])/production[2]), 
+			0
+		)
 		waitTime = timeToResource + 1
 		if waitTime < time:
 			cantBuildAnything = False
 			maxFound = max(
+				getMaxGeode(
+					time - waitTime,
+					(
+						production[0],
+						production[1],
+						production[2],
+						production[3] + 1
+					),
+					(
+						stock[0] + waitTime*production[0] - blueprint[3][0],
+						stock[1] + waitTime*production[1],
+						stock[2] + waitTime*production[2] - blueprint[3][1],
+						stock[3] + waitTime*production[3]
+					),
+					blueprint
+				),
+				maxFound
+			)
+	if cantBuildAnything:
+		maxFound = max(
 			getMaxGeode(
-				time - waitTime,
+				0,
 				(
 					production[0],
 					production[1],
 					production[2],
-					production[3] + 1
+					production[3]
 				),
 				(
-					stock[0] + waitTime*production[0] - blueprint[3][0],
-					stock[1] + waitTime*production[1],
-					stock[2] + waitTime*production[2] - blueprint[3][1],
-					stock[3] + waitTime*production[3]
+					stock[0] + time*production[0],
+					stock[1] + time*production[1],
+					stock[2] + time*production[2],
+					stock[3] + time*production[3]
 				),
 				blueprint
 			),
 			maxFound
-			)
-	if cantBuildAnything:
-		maxFound = max(
-		getMaxGeode(
-			0,
-			(
-				production[0],
-				production[1],
-				production[2],
-				production[3]
-			),
-			(
-				stock[0] + time*production[0],
-				stock[1] + time*production[1],
-				stock[2] + time*production[2],
-				stock[3] + time*production[3]
-			),
-			blueprint
-		),
-		maxFound
 		)
 	memo[memoKey] = maxFound
 	return maxFound
