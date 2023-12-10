07025b5eecbd0bb4a1086e8076117646cbc33f9a
Delta-de-Dirac <rconceicao.lucas@gmail.com>
2023-12-09 23:57:51 -0300

    added files for day 9

---
+++ 2023/9/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+
+
+func main(){
+	log.Println("Starting...")
+	if len(os.Args) != 2 {
+		log.Println("Program takes exactly one argument")
+		log.Fatalf("Usage: %s <input fileName>", os.Args[0])
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	return
+}
