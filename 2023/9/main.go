df9d7820d8956336aa088dbee45f256fd6d7e4ed
Delta-de-Dirac <rconceicao.lucas@gmail.com>
2023-12-19 02:24:05 -0300

    solution to day 9

--- 2023/9/main.go
+++ 2023/9/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 
+func isAllZero(s []int) bool{
+	for _, v := range(s){
+		if v != 0{
+			return false
+		}
+	}
+	return true
+}
 
 func main(){
 	log.Println("Starting...")
@@ -21,6 +32,72 @@ func main(){
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	scanner.Scan()
+	sum := 0
+	for scanner.Scan(){
+		lineSum := 0
+		fields := strings.Fields(scanner.Text())
+		nums := make([]int, 0)
+		for _, f := range(fields){
+			num, err := strconv.Atoi(f)
+			if err!=nil{
+				log.Fatal(err)
+			}
+			nums = append(nums, num)
+		}
+		lineSum += nums[len(nums)-1]
+		diff := make([]int, 0)
+		for i := range(nums[:len(nums)-1]){
+			diff = append(diff, nums[i+1] - nums[i])
+		}
+		lineSum += diff[len(diff)-1]
+		for ;!isAllZero(diff);{
+			n_diff := make([]int, 0)
+			for i := range(diff[:len(diff)-1]){
+				n_diff = append(n_diff, diff[i+1] - diff[i])
+			}
+			lineSum += n_diff[len(n_diff)-1]
+			diff = n_diff
+		}
+		sum += lineSum
+	}
+
+	log.Println("Result to part 1:", sum)
+	// part 2
+	file.Seek(io.SeekStart, 0)
+	scanner = bufio.NewScanner(file)
+	sum = 0
+	for scanner.Scan(){
+		lineSum := 0
+		fields := strings.Fields(scanner.Text())
+		nums := make([]int, 0)
+		for _, f := range(fields){
+			num, err := strconv.Atoi(f)
+			if err!=nil{
+				log.Fatal(err)
+			}
+			nums = append(nums, num)
+		}
+		for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+
+		lineSum += nums[len(nums)-1]
+		diff := make([]int, 0)
+		for i := range(nums[:len(nums)-1]){
+			diff = append(diff, nums[i+1] - nums[i])
+		}
+		lineSum += diff[len(diff)-1]
+		for ;!isAllZero(diff);{
+			n_diff := make([]int, 0)
+			for i := range(diff[:len(diff)-1]){
+				n_diff = append(n_diff, diff[i+1] - diff[i])
+			}
+			lineSum += n_diff[len(n_diff)-1]
+			diff = n_diff
+		}
+		sum += lineSum
+	}
+
+	log.Println("Result to part 2:", sum)
 	return
 }
