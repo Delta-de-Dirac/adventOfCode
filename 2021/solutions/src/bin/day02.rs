24190416091d313a3b00f89bc0fd8d7c20c71611
Delta-de-Dirac <rconceicao.lucas@gmail.com>
2024-08-09 19:47:06 -0300

    solution to day02 part 2 2021

--- 2021/solutions/src/bin/day02.rs
+++ 2021/solutions/src/bin/day02.rs
@@ -33,7 +33,39 @@ fn main(){
             }
         }
     }
+
     io::stdout()
         .write_all(format!{"Result part 1: {}\n", pos * dep}.as_bytes())
+        .expect("Error writing to stdout");
+
+    pos = 0;
+    dep = 0;
+    let mut aim = 0;
+    for line in input_file.lines() {
+        let words : Vec<&str> = line.split_whitespace().collect();
+        if let [command, arg] = &words[..]{
+            match *command {
+                "forward" => {
+                    pos += (*arg).parse::<i32>().expect(format!("cannot parse arg {arg}").as_str());
+                    dep += aim*(*arg).parse::<i32>().expect(format!("cannot parse arg {arg}").as_str());
+                },
+                "up" => {
+                    aim -= (*arg).parse::<i32>().expect(format!("cannot parse arg {arg}").as_str());
+                },
+                "down" => {
+                    aim += (*arg).parse::<i32>().expect(format!("cannot parse arg {arg}").as_str());
+
+                },
+                _ => {
+                    io::stdout()
+                    .write_all(format!{"Cannot parse command {command}\n"}.as_bytes())
+                    .expect("Error writing to stdout");
+                }
+            }
+        }
+    }
+
+    io::stdout()
+        .write_all(format!{"Result part 2: {}\n", pos * dep}.as_bytes())
         .expect("Error writing to stdout");
 }
