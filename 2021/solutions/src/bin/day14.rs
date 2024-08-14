2374bc0c306d328cd130f113514a70d1df4a0c74
Delta-de-Dirac <rconceicao.lucas@gmail.com>
2024-08-14 10:44:00 -0300

    finished day 14

--- 2021/solutions/src/bin/day14.rs
+++ 2021/solutions/src/bin/day14.rs
@@ -1,6 +1,60 @@
 use std::fs;
 use std::collections::HashMap;
 
+fn merge_hash(
+    x: HashMap<char, u64>,
+    mut y: HashMap<char, u64>
+) -> HashMap<char, u64> {
+    for (key, val_x) in x.iter() {
+        y.entry(*key).and_modify(|val_y| *val_y += val_x).or_insert(*val_x);
+    }
+    return y;
+}
+
+fn count_part2(
+    polymer: &Vec<char>,
+    polymap: &HashMap<[char; 2], char>,
+    t: u32
+    ) -> HashMap<char, u64>{
+
+    let mut memo : HashMap<(char, char, u32), HashMap<char, u64>> = HashMap::new();
+    let mut result : HashMap<char, u64> = HashMap::new();
+
+    for ch in polymer.iter() {
+        result.entry(*ch).and_modify(|v| *v += 1).or_insert(1);
+    }
+
+    for w in polymer.windows(2){
+        let l = w.get(0).expect("Invalid window");
+        let r = w.get(1).expect("Invalid window");
+
+        result = merge_hash(count_generated(l, r, t, polymap, &mut memo), result);
+    }
+    return result;
+}
+
+fn count_generated(
+  l: &char,
+  r: &char,
+  t: u32,
+  polymap: &HashMap<[char; 2], char>,
+  memo: &mut HashMap<(char, char, u32), HashMap<char, u64>>) -> HashMap<char, u64> {
+    if t == 0 {
+        return HashMap::new();
+    }
+    if let Some(result) = memo.get(&(*l, *r, t)){
+        return result.clone();
+    }
+    let mut result : HashMap<char, u64> = HashMap::new();
+    if let Some(c) = polymap.get(&[*l,*r]) {
+        result.insert(*c, 1);
+        result = merge_hash(count_generated(l, c, t-1, polymap, memo), result);
+        result = merge_hash(count_generated(c, r, t-1, polymap, memo), result);
+    }
+    memo.insert((*l, *r, t), result.clone());
+    return result;
+}
+
 fn grow(polymer: Vec<char>, polymap: &HashMap<[char; 2], char>) -> Vec<char>{
     let mut result : Vec<char> = Vec::new();
     for w in polymer.windows(2){
@@ -34,14 +88,14 @@ fn main(){
 
     let mut lines_i = input_lines.iter();
 
-    let mut polymer = lines_i
+    let polymer_input = lines_i
     .next()
     .expect("can't parse first line")
     .trim()
     .chars()
     .collect::<Vec<char>>();
 
-    let mut lines_i = lines_i.skip(1);
+    let lines_i = lines_i.skip(1);
 
     let mut polymap : HashMap<[char; 2], char> = HashMap::new();
 
@@ -64,6 +118,8 @@ fn main(){
         polymap.insert(key, value);
     }
 
+    let mut polymer = polymer_input.clone();
+
     for _ in 0..10 {
         polymer = grow(polymer, &polymap);
     }
@@ -82,5 +138,22 @@ fn main(){
 
     let result = max_val - min_val;
 
-    println!("Result part 1: {el_freq:#?}");
+    println!("Result part 1: {result}");
+
+    let polymer = polymer_input.clone();
+
+    let el_freq = count_part2(
+        &polymer,
+        &polymap,
+        40,
+    );
+
+    let freq_vals = el_freq.values();
+
+    let min_val = freq_vals.clone().min().expect("no elements in polymer");
+    let max_val = freq_vals.clone().max().expect("no elements in polymer");
+
+    let result = max_val - min_val;
+
+    println!("Result part 2: {result}");
 }
