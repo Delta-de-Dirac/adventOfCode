ace6af8fc65759d9fc53379cac61dc1bd12554ac
Delta-de-Dirac <rconceicao.lucas@gmail.com>
2024-08-21 14:50:35 -0300

    solution day 21 part 2

--- 2021/solutions/src/bin/day21.rs
+++ 2021/solutions/src/bin/day21.rs
@@ -1,7 +1,79 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn part1_die_gen(i: u32) -> u32 {
     return 3*(3*i + 1) + 3;
+}
+
+fn count_universe(
+    turn: bool,
+    p1: u8,
+    p2: u8,
+    s1: u8,
+    s2: u8,
+    memo: &mut HashMap::<(bool,u8,u8,u8,u8), [u64; 2]>
+) -> [u64; 2] {
+    if s1 >= 21 {
+        return [1, 0];
+    }
+    if s2 >= 21 {
+        return [0, 1];
+    }
+    if memo.contains_key(&(turn, p1, p2, s1, s2)) {
+        return memo.get(&(turn, p1, p2, s1, s2)).expect("").clone();
+    }
+    if memo.contains_key(&(!turn, p2, p1, s2, s1)) {
+        let inv_result = memo.get(&(!turn, p2, p1, s2, s1)).expect("").clone();
+        return [inv_result[1], inv_result[0]];
+    }
+    match turn {
+        false => {
+            let mut u1 = 0;
+            let mut u2 = 0;
+            for d1 in 1..=3 {
+                for d2 in 1..=3{
+                    for d3 in 1..=3{
+                        let [partial1, partial2] = count_universe(
+                            true,
+                            ((p1+(d1+d2+d3)-1)%10)+1,
+                            p2,
+                            s1+((p1+(d1+d2+d3)-1)%10)+1,
+                            s2,
+                            memo
+                        );
+                        u1 += partial1;
+                        u2 += partial2;
+                    }
+                }
+            }
+            let result = [u1, u2];
+            memo.insert((turn, p1, p2, s1, s2), result);
+            return result;
+        }
+        true => {
+            let mut u1 = 0;
+            let mut u2 = 0;
+            for d1 in 1..=3 {
+                for d2 in 1..=3{
+                    for d3 in 1..=3{
+                        let [partial1, partial2] = count_universe(
+                            false,
+                            p1,
+                            ((p2+(d1+d2+d3)-1)%10)+1,
+                            s1,
+                            s2+((p2+(d1+d2+d3)-1)%10)+1,
+                            memo
+                        );
+                        u1 += partial1;
+                        u2 += partial2;
+                    }
+                }
+            }
+            let result = [u1, u2];
+            memo.insert((turn, p1, p2, s1, s2), result);
+            return result;
+        }
+    }
 }
 
 fn main() {
@@ -20,8 +92,6 @@ fn main() {
     .trim()
     .split(|x| x == '\n')
     .collect::<Vec<&str>>();
-
-    const BOARD_SIZE : u32 = 10;
 
     let player_1_start : u32 = input_lines
                             .get(0)
@@ -65,7 +135,12 @@ fn main() {
         }
     }
 
-
     println!("Result part 1: {}", losing_score*rolls*3);
 
+    let mut memo = HashMap::<(bool,u8,u8,u8,u8), [u64; 2]>::new();
+    let [u1, u2] = count_universe(false, player_1_start as u8, player_2_start as u8, 0, 0, &mut memo);
+
+    let result = if u2 > u1 {u2} else {u1};
+
+    println!("Result part 2: {}", result);
 }
